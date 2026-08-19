0c11a8f3f5ac2a96ad8615f930a0cc82c16fbdba
smooth1994 <13380054262@163.com>
2026-08-19 13:53:53 +0800

    update(main)：创建初始项目

---
+++ JournalNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JournalNote
+//
+//  Created by mac on 2026/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JournalNoteTests/JournalNoteTests.swift
@@ -0,0 +1,19 @@
+//
+//  JournalNoteTests.swift
+//  JournalNoteTests
+//
+//  Created by mac on 2026/8/19.
+//
+
+import Testing
+@testable import JournalNote
+
+struct JournalNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
